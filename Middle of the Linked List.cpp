7692c1fa4e4dc0e3795df828e368c83ade231fbb
SudipMukherjee <90024985+SudipMukhejee@users.noreply.github.com>
2022-09-09 20:05:01 +0530

    Update Middle of the Linked List.cpp

--- Middle of the Linked List.cpp
+++ Middle of the Linked List.cpp
@@ -10,3 +10,19 @@ public:
 		return slow;
     }
 };
+/*
+amar moto kora😊😊😊😊😊😊
+Node *pre=head;
+        int count=0;
+        while(pre!=NULL){
+            count++;
+            pre=pre->next;
+        }
+        count /=2;
+        Node *post=head;
+        while(count!=0){
+            post=post->next;
+            count--;
+        }
+        return post->data;
+	*/
